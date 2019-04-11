a41070f288956730e5341346583a6a086bed0b2c
Jan Küster <jkuester@uni-bremen.de>
2019-04-11 15:20:15 +0200

    published 1.0.0

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Install NPM packages on the run to test packages with NPM dependencies.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jankapunkt/meteor-test-npm-deps.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
